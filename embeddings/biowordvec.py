93bd0f7913ff1940b103cae9765ca57ded0fb642
antoinepayan <antoine.payan@hotmail.fr>
2019-10-01 16:20:16 +0200

    BioWordVec embedding

--- embeddings/biowordvec.py
+++ embeddings/biowordvec.py
@@ -1,5 +1,8 @@
 # Libraries
-
+import os
+import pandas as pd
+import numpy as np
+from gensim.models.keyedvectors import KeyedVectors
 
 # Constants
 
@@ -7,23 +10,54 @@ OUTPUT_FORMAT = {
     'n_cols': 512
 }
 
+TRAINED_MODEL = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'bin/bio_embedding_extrinsic')
+
 
 # Core functions
 
-def embed_text(data, output_format=None, columns=None):
+def get_vect(word, model):
+    """
+    :param word: word to be embedded
+    :param model: model used to embed a word
+    :return: word vector
+    """
+    try:
+        return model.get_vector(word)
+    except KeyError:
+        return np.zeros((model.vector_size,))
+
+
+def sum_vectors(sentence, model):
+    """
+    :param sentence: sentence to be embedded
+    :param model: model used to embed a word
+    :return: vector
+    """
+    return sum(get_vect(w, model) for w in sentence)
+
+
+def word2vec_features(sentences, model):
+    """
+    :param sentences: sentences to be embedded
+    :param model: model used to embed a word
+    :return: numpy list of list
+    """
+    feats = np.vstack([sum_vectors(p, model) for p in sentences])
+    return feats
+
+
+def embed_text(abstracts, output_format=None):
+    """
+    :param abstracts: pandas Series of abstracts
+    :param output_format: dict specifying output format of the embedding method
+    :return: embedding and associated format
+    """
 
     if output_format is None:
         output_format = OUTPUT_FORMAT
 
-    features = data.copy()
+    word_vectors = KeyedVectors.load_word2vec_format(TRAINED_MODEL, binary=True)
 
-    if columns is not None:
-        features = features[[columns]]
-
-    embedding = ...
-
-    """
-    Embedding method
-    """
+    embedding = pd.DataFrame(word2vec_features(abstracts, word_vectors))
 
     return embedding, output_format
