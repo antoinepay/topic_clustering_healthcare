af0c1da98dec1908e0292924a9a53839fc7603f2
Marie Laval <marie.laval@hec.edu>
2019-10-02 11:15:44 +0200

    kmeans function

--- main.py
+++ main.py
@@ -3,7 +3,15 @@
 import os
 import pandas as pd
 
+from sklearn.cluster import KMeans
+
 from embeddings import biowordvec
+from embeddings import google_sentence_embedding
+
+
+# Constants
+
+random_state = 42
 
 abstracts_path = 'data/abstracts.csv'
 
@@ -22,5 +30,36 @@ if not os.path.exists(abstracts_path):
 
 abstracts = pd.read_csv(abstracts_path)
 
-vectors, format = biowordvec.embed_text(abstracts.dropna().text)
+
+# Core functions
+
+def make_kmeans(embedding_type, n_clusters):
+    """
+    :param embedding_type: str either 'biowordvec' or 'guse'
+    :param n_clusters: int number of clusters
+    :return:
+    """
+
+    if embedding_type == "biowordvec":
+        vectors, format = biowordvec.embed_text(abstracts.dropna().text)
+    elif embedding_type == "guse":
+        vectors, format = google_sentence_embedding.embed_text(abstracts.dropna().text)
+    else:
+        raise Exception("embedding_type should be either biowordvec or guse")
+
+    vectors = pd.DataFrame(vectors.apply(pd.Series))
+
+    kmeans = KMeans(n_clusters=n_clusters, random_state=random_state).fit(vectors)
+
+    clusters = pd.concat(
+        [
+            vectors,
+            pd.DataFrame(
+                [str(i) for i in kmeans.predict(vectors)],
+                columns=('cluster',))
+        ],
+        axis=1
+    )
+
+    return clusters
 
