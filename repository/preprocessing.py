58b4d09fc4e5c29c78a2554860b8860b9a0b5a3c
Rick Schneider <schneider.rick@hotmail.com>
2019-10-03 14:09:52 +0200

    structured preprocessing for pipeline

--- repository/preprocessing.py
+++ repository/preprocessing.py
@@ -1,34 +1,51 @@
+
 # Libraries
 
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.tokenize.treebank import TreebankWordDetokenizer
+import pandas as pd
 
 
 # Mandatory downloads
-
 def nltk_package_downloads():
     nltk.download('punkt')
     nltk.download('stopwords')
 
 
-def full_preprocessing(df):
+# word preprocessing
+def launch_preprocessing_words(df):
     """
     :return: a preproceessed data frame
     """
     df = df.rename(columns={"Tiltle": "title"})
     df = df.dropna(subset=['text', 'title'])
 
-    df["tokens"] = df["text"].apply(preprocessing)
-    df["title_clean"] = df["title"].apply(preprocessing)
+    df["tokens"] = df["text"].apply(preprocessing, args=["text"])
+    df["title_clean"] = df["title"].apply(preprocessing, args=["title"])
+
+    # modDfObj = dfObj.apply(multiplyData, args=[4])
 
     final = detokenize(df, "tokens")
 
     return final
 
 
-def preprocessing(column):
+# sentence preprocessing
+def launch_preprocessing_sentence(df):
+    """
+    :return: a preproceessed data frame
+    """
+    df = df.rename(columns={"Tiltle": "title"})
+    df = df.dropna(subset=['text', 'title'])
+
+    df["title_clean"] = df["title"].apply(preprocessing, args=["title"])
+
+    return df
+
+
+def preprocessing(column, prep_type):
     """
     :param column: column to preprocess
     :return: a pre-processed column
@@ -40,19 +57,7 @@ def preprocessing(column):
     # Deleting words with  only one caracter
     tokens = [token for token in tokens if len(token) > 2]
 
-    # stopwords + lowercase
-    normal_stopwords = stopwords.words('english')
-
-    #     import more extensive stopwords + convert to list the first column
-    comprehensive_stopwords = \
-    pd.read_csv('https://raw.githubusercontent.com/Alir3z4/stop-words/master/english.txt', header=None)[0].tolist()
-    #     potential further stopwords (will have to test that)
-    special_medical = ["complex", "patients", "treatment", "months",
-                       "rate", "prevalence", "case", "early", "management",
-                       "reported", "information", "baseline", "study", "questionnaire", "results", "month", "months",
-                       "years", "year"]
-
-    stopW = normal_stopwords + comprehensive_stopwords + special_medical
+    stopW = stopword_list(prep_type)
 
     tokens = [token.lower() for token in tokens if token.lower() not in stopW]
 
@@ -66,7 +71,34 @@ def preprocessing(column):
     return tokens
 
 
-def detokenize(df_tokens,name_token_column):
+def stopword_list(prep_type):
+    # stopwords + lowercase
+    normal_stopwords = stopwords.words('english')
+
+    #     import more extensive stopwords + convert to list the first column
+    """
+    NOT WORKING YET - needs to be stored
+
+    comprehensive_stopwords = \
+    pd.read_csv('https://raw.githubusercontent.com/Alir3z4/stop-words/master/english.txt', header=None)[0].tolist()
+
+    """
+    #     potential further stopwords (will have to test that)
+    special_medical = ["complex", "patients", "treatment", "months",
+                       "rate", "prevalence", "case", "early", "management",
+                       "reported", "information", "baseline", "study", "questionnaire", "results", "month", "months",
+                       "years", "year"]
+
+    if prep_type == "text":
+        stopW = normal_stopwords
+        # for the title, we also want to remove individual special words
+    else:
+        stopW = normal_stopwords + special_medical
+
+    return stopW
+
+
+def detokenize(df_tokens, name_token_column):
     """
     :param df_tokens: dataframe with a tokenized column that will be detokenized
     :param name_token_column: string with the name of the column holding the tokenized data
