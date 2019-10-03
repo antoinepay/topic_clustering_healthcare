df0ae13cf10bf688a5cb14f5c8226d89edaf78ec
lo1gur <louisgruand@gmail.com>
2019-10-03 10:09:05 +0200

    added tfidif info

--- TFIDF.py
+++ TFIDF.py
@@ -1,7 +1,40 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
 
 
 # website to look @ for potential info: https://www.freecodecamp.org/news/how-to-process-textual-data-using-tf-idf-in-python-cd2bbc0a94a3/
 
 # what does tokenizer do and do we need it?
-vectorizer = TfidfVectorizer(tokenizer=word_tokenize)
+
+
+abstracts = pd.read_excel('data/abstracts_pubmed.xlsx')
+abstracts.head()
+abstracts.columns
+abstracts.tokens[5]
+
+
+
+# dont need tokenizer parameter bc we've already tokenized
+vectorizer = TfidfVectorizer()
+response = vectorizer.fit_transform((abstracts.tokens))
+
+
+feature_names_en = np.array(vectorizer.get_feature_names())
+sorted_tfidf_index_en = response.max(0).toarray()[0].argsort()
+
+print('Smallest tfidf en:\n{}\n'.format(feature_names_en[sorted_tfidf_index_en[:10]]))
+
+df_sklearn_total_en = pd.DataFrame(response.todense(), columns = feature_names_en)
+df_sklearn_total_en.head()
+
+
+
+
+
+
+
+# have to get rid of empty tokens
+# have to get rid of p
+# have to get rid of n
+'    '
+'   '
